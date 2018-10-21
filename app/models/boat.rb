eba9c30e598ba0c7709b9c8fc7661caa45b785f2
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-21 21:04:32 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,6 +28,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    includes(:classifications).where('classifications.count = 3')
+    where('classifications.count = 3')
   end
 end
