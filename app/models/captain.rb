00f9a8c41849c7cfca56bbc18171d066dab0d93c
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-21 22:08:04 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -7,9 +7,10 @@ class Captain < ActiveRecord::Base
 
   def self.sailors
     includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+    # User.where({ name: ["Alice", "Bob"]})
   end
 
   def self.talented_seafarers
-    includes(boats: :classifications).where(classifications: {name: ["Motorboats", "Sailboats"]})
+    includes(boats: :classifications).where(classifications: {name: ["Motorboat", "Sailboat"]})
   end
 end
